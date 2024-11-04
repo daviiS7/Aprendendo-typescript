9b7dec67f9e6186a1bcff142fc69504c9bbfffcc
Davi Santos <155538912+daviiS7@users.noreply.github.com>
2024-11-04 20:47:31 -0300

    Add files via upload

---
+++ Curso de typescript/Part 1/index.js
@@ -0,0 +1,9 @@
+var FirstName = "Davi";
+var anotherName = 1;
+var x = true;
+function greeting(name) {
+    console.log("Olá" + name);
+}
+greeting(FirstName);
+// greeting(anotherName)
+// greeting(x)
---
+++ Curso de typescript/Part 1/index.ts
@@ -0,0 +1,11 @@
+const FirstName = "Davi"
+const anotherName = 1
+const x = true
+
+function greeting(name: string) {
+    console.log("Olá" + name)
+}
+
+greeting(FirstName)
+// greeting(anotherName)
+// greeting(x)
