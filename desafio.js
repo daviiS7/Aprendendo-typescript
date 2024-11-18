08c0698499bef5aa412260a0828f885055f8323d
Davi Santos <155538912+daviiS7@users.noreply.github.com>
2024-11-18 19:21:49 -0300

    Add files via upload

---
+++ desafio.js
@@ -0,0 +1,5 @@
+function somarvalores(n1, n2) {
+    return n1 + n2;
+}
+var resultado = somarvalores(300, 700);
+console.log("A soma dos valores é igual a", resultado);
---
+++ desafio.ts
@@ -0,0 +1,6 @@
+function somarvalores(n1: number, n2: number){
+    return n1 + n2
+}
+
+const resultado = somarvalores(300, 700);
+console.log("A soma dos valores é igual a", resultado);
